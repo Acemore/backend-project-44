d2b5cde32c2645aec31d4d93d6dba8f8357ea381
Sergey Martynov <acemore@mail.ru>
2023-04-02 16:51:14 +0300

    Stand out games engine

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+
+const roundsCount = 3;
+
+const playGame = (game) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+
+  console.log(game.rules);
+
+  for (let roundNumber = 0; roundNumber < roundsCount; roundNumber += 1) {
+    const num = game.getQuestion();
+    console.log(`Question: ${num}`);
+
+    const userAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = String(game.getAnswer(num));
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default playGame;
