ad914e48f54191829112794eb3f62ca89a5f1d86
Sergey Martynov <acemore@mail.ru>
2023-04-03 00:21:13 +0300

    Update interaction with random data

--- src/random-data.js
+++ src/random-data.js
@@ -3,7 +3,12 @@ import _ from 'lodash';
 const randomNumsGeneratorMinValue = 0;
 const randomNumsGeneratorMaxValue = 100;
 
-const getRandomInt = () => _.random(randomNumsGeneratorMinValue, randomNumsGeneratorMaxValue);
+const getRandomInt = (min, max) => _.random(min, max);
 const getRandomOperator = () => _.sample(['+', '-', '*']);
 
-export { getRandomInt, getRandomOperator };
+export {
+  getRandomInt,
+  getRandomOperator,
+  randomNumsGeneratorMinValue,
+  randomNumsGeneratorMaxValue,
+};
