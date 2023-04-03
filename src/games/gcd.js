c873f671900b0691769f424f1d1a4d9b3a3c7db7
Sergey Martynov <acemore@mail.ru>
2023-04-03 11:29:30 +0300

    Fix gcd game

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -13,11 +13,15 @@ const getQuestionAndAnswer = () => {
 
   const nums = `${firstNum} ${secondNum}`;
 
-  let gcd = 1;
-  for (let num = 2; num <= minNum; num += 1) {
-    if (firstNum % num === 0 && secondNum % num === 0) {
-      gcd = num;
+  let gcd;
+  if (firstNum !== 0 && secondNum !== 0) {
+    for (let num = 2; num <= minNum; num += 1) {
+      if (firstNum % num === 0 && secondNum % num === 0) {
+        gcd = num;
+      }
     }
+  } else {
+    gcd = Math.max(firstNum, secondNum);
   }
 
   return [nums, String(gcd)];
